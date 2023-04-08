b9a4713a90f25cd4c4c3a564ad71c531f2d89b84
Sprok Dániel <sprokdaniel@gmail.com>
2023-04-08 16:06:36 +0200

    Regisztrációs felület bővíése

--- app/src/main/java/com/example/electronics_service/MainActivity.java
+++ app/src/main/java/com/example/electronics_service/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.electronics_service;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = Objects.requireNonNull(MainActivity.class.getPackage()).toString();
+    private static final int SECRET_KEY = 18;
 
     EditText userNameET;
     EditText passwordET;
+
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
         userNameET = findViewById(R.id.editTextUserName);
         passwordET = findViewById(R.id.editTextPassword);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
     }
 
     public void login(View view) {
         String userName = userNameET.getText().toString();
         String password = passwordET.getText().toString();
 
-        Log.i(LOG_TAG, "Logged in: " + userName + ", password: " + password);
+        Log.i(LOG_TAG, "Logged in user: " + userName + ", Password: " + password);
     }
 
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
-        intent.putExtra("SECRET_KEY", 18);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userNameET.getText().toString());
+        editor.putString("password", passwordET.getText().toString());
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/electronics_service/RegisterActivity.java
+++ app/src/main/java/com/example/electronics_service/RegisterActivity.java
@@ -2,19 +2,30 @@ package com.example.electronics_service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
-public class RegisterActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final String PREF_KEY = Objects.requireNonNull(RegisterActivity.class.getPackage()).toString();
+
     EditText lastNameET;
     EditText firstNameET;
     EditText userEmailET;
     EditText userNameET;
     EditText passwordET;
     EditText passwordConfirmET;
+    EditText phoneNumberET;
+    Spinner spinner;
+    EditText addressET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +34,33 @@ public class RegisterActivity extends AppCompatActivity {
 
         int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
 
+        if (secret_key != 18) {
+            finish();
+        }
+
         lastNameET = findViewById(R.id.editTextLastName);
         firstNameET = findViewById(R.id.editTextFirstName);
         userEmailET = findViewById(R.id.editTextUserEmail);
         userNameET = findViewById(R.id.editTextUserName);
         passwordET = findViewById(R.id.editTextPassword);
         passwordConfirmET = findViewById(R.id.editTextPasswordConfirm);
+        phoneNumberET = findViewById(R.id.editTextPhoneNumber);
+        spinner = findViewById(R.id.phoneSpinner);
+        addressET = findViewById(R.id.editTextAddress);
 
-        if (secret_key != 18) {
-            finish();
-        }
+        SharedPreferences preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        userNameET.setText(userName);
+        passwordET.setText(password);
+        passwordConfirmET.setText(password);
+
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.phone_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     public void register(View view) {
@@ -44,15 +72,32 @@ public class RegisterActivity extends AppCompatActivity {
         String passwordConfirm = passwordConfirmET.getText().toString();
 
         if (!password.equals(passwordConfirm)) {
-            Log.i(LOG_TAG, "Given passwords don't match!");
+            Log.e(LOG_TAG, "Given passwords don't match!");
+            return;
         }
-        else {
-            Log.i(LOG_TAG, "Registered: " + userName + ", Full name: " + lastName + " " + firstName +
-                    ", e-mail: " + userEmail + ", password: " + password);
-        }
+
+        String phoneNumber = phoneNumberET.getText().toString();
+        String phoneType = spinner.getSelectedItem().toString();
+        String address = addressET.getText().toString();
+
+        Log.i(LOG_TAG, "Registered user: " + userName + ", Full name: " + lastName + " " + firstName +
+                ", E-mail: " + userEmail + ", Password: " + password + ", Phone number: " + phoneNumber +
+                ", Phone Type: " + phoneType + ", Address: " + address);
+
     }
 
     public void cancel(View view) {
         finish();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String selectedItem = adapterView.getItemAtPosition(i).toString();
+        Log.i(LOG_TAG, selectedItem);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
