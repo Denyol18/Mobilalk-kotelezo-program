86d1426ccf467bbe6c9b2172688446ce8f961e98
Sprok Dániel <sprokdaniel@gmail.com>
2023-04-11 20:30:48 +0200

    noti permission

--- app/src/main/java/com/example/electronics_service/HomeActivity.java
+++ app/src/main/java/com/example/electronics_service/HomeActivity.java
@@ -1,10 +1,16 @@
 package com.example.electronics_service;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +22,8 @@ public class HomeActivity extends AppCompatActivity {
     private static final String LOG_TAG = HomeActivity.class.getName();
     private FirebaseUser user;
     private FirebaseAuth mAuth;
+
+    final int PERMISSION_REQUEST_CODE = 112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,48 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         mAuth = FirebaseAuth.getInstance();
+
+        SharedPreferences pref = this.getSharedPreferences("PACKAGE.NAME",MODE_PRIVATE);
+        boolean firstTime = pref.getBoolean("firstTime",true);
+        if(firstTime){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (!shouldShowRequestPermissionRationale("112")){
+                    getNotificationPermission();
+                }
+            }
+
+            pref.edit().putBoolean("firstTime",false).apply();
+        }
+
     }
+
+    public void getNotificationPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.POST_NOTIFICATIONS},
+                    PERMISSION_REQUEST_CODE);
+            }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    new AlertDialog.Builder(this)
+                            .setTitle("Értesítések").setMessage("Nem fog kapni értesítéseket időpont foglalás, törlés és" +
+                                    "sürgőssé megjelölés után!")
+                            .setPositiveButton("OK", null).show();
+                }
+                return;
+
+        }
+    }
+
 
     public void toAppointmentBook(View view) {
         Intent intent = new Intent(this, AppointmentBookActivity.class);
